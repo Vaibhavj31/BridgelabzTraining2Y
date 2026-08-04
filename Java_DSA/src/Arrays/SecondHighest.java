3abdd160aad4dd3c2b328772cb5de2d0bd3fea7e
navyag4 <navyagupta6176@gmail.com>
2026-08-04 15:49:06 +0530

    First Commit

---
+++ Java_DSA/src/Arrays/SecondHighest.java
@@ -0,0 +1,19 @@
+public class SecondHighest {
+    static int secondHighest(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                second = max;
+                max = arr[i];
+            }else if(arr[i]>second && arr[i]!=max){
+                second = arr[i];
+            }
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 2, 15, 7, 3};
+        System.out.println("Second Highest = " + secondHighest(arr));
+    }
+}
---
+++ Java_DSA/src/Arrays/swap.java
@@ -0,0 +1,14 @@
+public class swap{
+    static void swap(int[] arr){
+        int temp=arr[0];
+        arr[0]=arr[4];
+        arr[4]=temp;
+    }
+        public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7};
+            swap(arr);
+            for(int i=0; i<arr.length; i++){
+                System.out.print(arr[i]+" ");
+            }
+        }
+}
