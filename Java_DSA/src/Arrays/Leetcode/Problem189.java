c7dacd28d6269a43e9f2c80279df176cdd6f39a1
navyag4 <navyagupta6176@gmail.com>
2026-08-05 09:52:44 +0530

    First Commit

---
+++ Java_DSA/src/Arrays/Leetcode/Problem189.java
@@ -0,0 +1,20 @@
+package Arrays.Leetcode;
+
+public class Problem189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int n = nums.length - 1;
+        reverse(nums, 0, n);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
