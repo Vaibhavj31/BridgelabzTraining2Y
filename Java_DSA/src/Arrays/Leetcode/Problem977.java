b9aa3abea81c5aa8bc4871cfe851036ce2144927
navyag4 <navyagupta6176@gmail.com>
2026-08-11 15:52:10 +0530

    First Commit

---
+++ Java_DSA/src/Arrays/Leetcode/Problem977.java
@@ -0,0 +1,22 @@
+package Arrays.Leetcode;
+
+import java.util.*;
+
+public class Problem977 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++)
+            nums[i] = nums[i] * nums[i];
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
